8c48da0ca5678ff292f20643fe54880891d00f20
Sampath Palla <pallasampathyadav@gmail.com>
2016-11-13 13:57:08 -0500

    First Commit
    First Commit

---
+++ Job Application/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Job_Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This project is a sample job search engine and application wizard without a database support.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Job Application/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Job_Application.MvcApplication" Language="C#" %>
